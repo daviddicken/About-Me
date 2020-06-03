926abbf96033b0ef063b46e0939d6a9e8e13c89d
daviddicken <davidfromseattle@gmail.com>
2020-06-02 17:54:09 -0700

    finished app.js

---
+++ temp.js
@@ -0,0 +1,86 @@
+//========= didn't read that they were suppose to be yes or no questions, will save the code below for later========
+//===================================================================================================
+
+//var kids, age, pets, dog, fish, correct = (0);
+  
+//   kids = prompt("How many kids do you think I have?\n\n\nclue: This is not the Brady Bunch");
+//   if(kids === '3')
+//   {
+//     alert("Correct");
+//     //console.log("Correct");
+//     correct++;
+//   }
+//   else
+//   {
+//     alert("Sorry I have three kids.\nAges 4,8,10");
+//     //console.log("Sorry I have three kids.\nAges 4,8,10");
+//   }
+//   //===========================================================================================================
+//   age = prompt("How old do you think I am?\n\n\nclue: careful now....");
+
+//   if(age === '41')
+//   {
+//     alert("Correct");
+//     //console.log("Correct");
+//     correct++;
+//   }
+//   else if(age > '41')
+//   {
+//     alert("What?! " + age + "!!! \nI'm only 41." );
+//     //console.log("What?! " + age + "!!! \nI'm only 41." );
+//   }
+//   else if(age < '41')
+//   {
+//     alert("Thank you..\nI'm actually 41\n\nI moisturize.");
+//     //console.log("Thank you..\nI'm actually 41\n\nI moisturize.");
+//   }
+//   else
+//   {
+//     alert("Yeah I'm not even sure where you going with " + age + " but I'm 41.");
+//     //console.log(`Yeah I'm not even sure where you going with ${age} but I'm 41.`);
+//   }
+
+// //=================================================================================================
+//   pets = prompt("Do you think I have any pets?").toLowerCase();
+//   console.log("pets " + pets);
+
+//   if(pets === 'yes')
+//   {
+//     alert("Correct");
+//     //console.log("Correct");
+//     correct++;
+//   }
+//   else
+//   {
+//     alert("I do have pets.");
+//     //console.log("I do have pets.");
+//   }
+// //==================================================================================================
+//   dog = prompt("So since you know I have pets, \nDo you think I'm a dog person or a cat person?").toLowerCase();
+//   console.log("dog " + dog)
+
+//   if(dog === 'dog')
+//   {
+//     alert("Correct! \n\nI have a 8 month old Labordoodle.");
+//     //console.log("Correct");
+//     correct++;
+//   }
+//   else
+//   {
+//     alert("No, I'm a dog person. \nI have a 8 month old Labordoodle.");
+//     //console.log("No, I'm a dog person. \nI have a 8 month old Labordoodle.");
+//   }
+// //==================================================================================================
+// fish = prompt("Can you guess what other type of animal I keep as a pet?").toLowerCase();
+// console.log("fish  " + fish);
+// if(fish === 'fish')
+// {
+//   alert("Correct!\n\nI have a saltwater reef aquarium");
+//   //console.log("Correct!\n\nI have a saltwater reef aquarium");
+//   correct++;
+// }
+// else
+// {
+//   alert("Nope the answer is fish.");
+//   //console.log("Nope the answer is fish.");
+// }
